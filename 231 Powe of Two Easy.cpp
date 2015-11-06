2690f03d3dbd4077e28fca238790082d45181d39
Xinyi <xie_xin_yi@hotmail.com>
2015-11-06 14:04:48 -0800

    Pay attention to binary search MIDDLE

--- 231 Powe of Two Easy.cpp
+++ 231 Powe of Two Easy.cpp
@@ -1,1 +1,14 @@
 // 231 Power of Two
+// Given an integer, write a function to determine if it is a power of two
+// binary search
+bool isPowerOfTwo(int n) {
+	int start = 0, end = n;
+	while(start < end){
+		int middle = (end - start)/2 + start;
+		if(middle * middle == n) return true;
+		if(pow(middle, 2) > n) start = middle + 1;
+		else end = middle - 1;
+	}	
+	return false;
+	 
+	}
