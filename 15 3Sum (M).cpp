66effa8639475eeca14c3b716a6580e5ecbfbd00
Xinyi <xie_xin_yi@hotmail.com>
2015-11-06 20:01:50 -0800

    Point Method

--- 15 3Sum (M).cpp
+++ 15 3Sum (M).cpp
@@ -2,18 +2,14 @@
  15 3sum Medium
  Input: {-1 0 1 2 -1 -4}
  Output: (-1 0 1), (-1 -1 2)
+ 
+ Three pointer: one is start, one is end, one is moving in the middle
 */
 vector<vector<int>> threeSum(vector<int>& nums) {
 	if(nums.size() < 3) return {};
 	sort(nums.begin(), nums.end());
   
-	vector<int> neg, pos;
-	bool flag = false;
 	
-	for(int i = 0; i < nums.size(); i++){
-		if(nums[i] < 0) neg.push_back(-nums[i]);
-		else if(nums[i] == 0) flag = true;
-        else pos.push_back(nums[i]);		
-	}
+	
 	
 }
