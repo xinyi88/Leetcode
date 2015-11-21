e58af6a62d726133523afcb7281bd160a4b471f8
Xinyi <xie_xin_yi@hotmail.com>
2015-11-20 16:27:49 -0800

    Two points method

---
+++ Minimum Size Subarray Sum.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+      int res = 0, start = 0, end = start;
+	  int n = nums.size();
+	  int sum = 0;
+	 
+	  
+    }
+};
